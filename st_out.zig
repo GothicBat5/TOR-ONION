39812a1ac710e5110dd75b88c9d937354508406d
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-17 15:03:18 +0800

    Update st_out.zig

--- st_out.zig
+++ st_out.zig
@@ -6,13 +6,13 @@ pub fn main() !void {
 
     try stdout.print("Input: ", .{});
 
-    var input_buffer: [256]u8 = undefined; // fixed buffer for input
+    var input_buffer: [256]u8 = undefined; //buffer for input
     const input = try stdin.readUntilDelimiterOrEof(&input_buffer, '\n');
 
     try stdout.print("\n : ", .{});
 
     for (input) |c| {
-        // Print each character as 8-bit binary
+
         try stdout.print("{b:0>8} ", .{c});
     }
 
